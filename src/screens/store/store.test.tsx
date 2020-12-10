e0965c0798a0ac9a27d57b4c7ac3c37d271cc06b
alejandroclvi <mcalvinolaguardia@gmail.com>
2020-12-10 06:30:59 -0500

    structure project

--- src/screens/store/store.test.tsx
+++ src/screens/store/store.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Store from './store';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Store />);
+  const linkElement = screen.getByText(/store/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/screens/store/store.tsx
@@ -0,0 +1,11 @@
+import './store.css';
+
+function Store() {
+  return (
+    <div className="Store">
+      <h2>Undefined Store</h2>
+    </div>
+  );
+}
+
+export default Store;
